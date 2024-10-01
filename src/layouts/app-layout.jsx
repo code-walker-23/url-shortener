013ea21830f73e2a9031a18912e5fba24c6b5351
code-walker-23 <chhabravinay549@gmail.com>
2024-10-01 19:31:52 +0530

    default-dark-theme

--- src/layouts/app-layout.jsx
+++ src/layouts/app-layout.jsx
@@ -2,13 +2,14 @@ import { Outlet } from "react-router-dom"
 const AppLayout = () => {
   return (
     <div>
-      <main>
+      <main className="min-h-screen container">
         {/* Header */}
-        {/* Body */}
         <Outlet/>
       </main>
 
-      {/* Footer */}
+      <div className="p-10 text-center bg-gray-800 mt-10">
+        Made with ❤️ by Vinay Chhabra
+      </div>
     </div>
   )
 }
