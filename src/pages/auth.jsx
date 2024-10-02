20d653d0a8d8846e4f9137f9012c0ee974e56f02
code-walker-23 <chhabravinay549@gmail.com>
2024-10-02 11:48:27 +0530

    building-login-page

--- src/pages/auth.jsx
+++ src/pages/auth.jsx
@@ -1,5 +1,27 @@
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { useSearchParams } from "react-router-dom";
+
 const Auth = () => {
-  return <div>Auth</div>;
+  const [searchParam] = useSearchParams();
+  return (
+    <div className="mt-36 flex flex-col items-center gap-10">
+      <h1 className="text-5xl font-extrabold">
+        {searchParam.get("createNew")
+          ? "Hold up! Let's login first"
+          : "Login / SignUp"}
+      </h1>
+      <Tabs defaultValue="login" className="w-[400px]">
+        <TabsList className = "grid w-full grid-cols-2">
+          <TabsTrigger value="login">Login</TabsTrigger>
+          <TabsTrigger value="signup">SignUp</TabsTrigger>
+        </TabsList>
+        <TabsContent value="login">
+          Make changes to your account here.
+        </TabsContent>
+        <TabsContent value="signup">Change your password here.</TabsContent>
+      </Tabs>
+    </div>
+  );
 };
 
 export default Auth;
